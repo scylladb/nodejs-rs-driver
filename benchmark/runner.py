05b05b2b1358a9a023c41bb57ebc54ce8ab0b0e7
Stapox35 <stapox35@gmail.com>
2025-05-06 14:13:06 +0200

    Add new benchmark - serialization types
    uuid, int, timeuuid, inet, date, time

--- benchmark/runner.py
+++ benchmark/runner.py
@@ -61,6 +61,7 @@ n_min["insert.js"] = 400_000 / 64
 n_min["select.js"] = 100_000 / 64
 n_min["deser.js"] = 2_000 / 64
 n_min["concurrent_deser.js"] = 2_000 / 64
+n_min["ser.js"] = 900 / 64
 n_min["batch.js"] = 3_000_000 / 64
 n_min["paging.js"] = 10_000 / 64
 n_min["large_select.js"] = 4_000 / 64
@@ -73,7 +74,8 @@ step = 4
 libs = ["scylladb-nodejs-rs-driver", "cassandra-driver"]
 benchmarks = ["concurrent_insert.js", "insert.js", "select.js",
              "concurrent_select.js", "batch.js", "paging.js",
-             "large_select.js", "deser.js", "concurrent_deser.js"]
+             "large_select.js", "deser.js", "concurrent_deser.js", 
+             "ser.js"]
 
 name_rust = {}
 name_rust["concurrent_insert.js"] = "concurrent_insert_benchmark"
@@ -82,6 +84,7 @@ name_rust["select.js"] = "select_benchmark"
 name_rust["concurrent_select.js"] = "concurrent_select_benchmark"
 name_rust["deser.js"] = "deser_benchmark"
 name_rust["concurrent_deser.js"] = "concurrent_deser_benchmark"
+name_rust["ser.js"] = "ser_benchmark"
 name_rust["batch.js"] = "batch_benchmark"
 name_rust["paging.js"] = "paging_benchmark"
 name_rust["large_select.js"] = "large_select_benchmark"
@@ -156,12 +159,12 @@ for ben in benchmarks:
 
 libs.append("rust-driver")
 
-cols = 3
+cols = 4
 rows_time = (len(df) + cols - 1) // cols
 rows_mem = (len(df_mem) + cols - 1) // cols
 total_rows = rows_time + rows_mem
 
-fig, axes = plt.subplots(total_rows, cols, figsize=(15, 5 * total_rows),
+fig, axes = plt.subplots(total_rows, cols, figsize=(20, 5 * total_rows),
                          facecolor="white")
 axes = axes.flatten()
 
