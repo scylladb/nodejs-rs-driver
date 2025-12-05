scylladb-zpp-2024-javascript-driver/scylladb-javascript-driver

Create Query executor
This executor exposes a rust callback (method on this struct),
that allows to retrieve following pages, without needing to pass
the statement and parameters of the query multiple times.

While for now we use Arc, that requires cloning on each use,
we may consider using Cow here, assuming there are no limitations here.

lib/client.js
@@ -590,6 +590,7 @@ class Client extends events.EventEmitter {
                 pageState,
             );
         }
+        let executor = result[2];
         /**
          * @param {Array<rust.PagingStateResponseWrapper|rust.QueryResultWrapper>} result
          * Tuple of page state response and query result
@@ -602,13 +603,10 @@ class Client extends events.EventEmitter {
             let resultSet = new ResultSet(result[1], result[0]);
             if (result[0].hasNextPage()) {
                 resultSet.nextPageAsync = async (pageState) => {
-                    return (
-                        await this.#rustyPaged(
-                            query,
-                            params,
-                            execOptions,
-                            pageState,
-                        )
+                    return parseResult(
+                        await executor.getNextPage(
+                            rust.PagingStateWrapper.fromBuffer(pageState),
+                        ),
                     )[1];
                 };
             }

scylladb/nodejs-rs-driver

lib/client.js
@@ -590,6 +590,7 @@ class Client extends events.EventEmitter {
                 pageState,
             );
         }
+        let executor = result[2];
         /**
          * @param {Array<rust.PagingStateResponseWrapper|rust.QueryResultWrapper>} result
          * Tuple of page state response and query result
@@ -602,13 +603,10 @@ class Client extends events.EventEmitter {
             let resultSet = new ResultSet(result[1], result[0]);
             if (result[0].hasNextPage()) {
                 resultSet.nextPageAsync = async (pageState) => {
-                    return (
-                        await this.#rustyPaged(
-                            query,
-                            params,
-                            execOptions,
-                            pageState,
-                        )
+                    return parseResult(
+                        await executor.fetchNextPage(
+                            rust.PagingStateWrapper.fromBuffer(pageState),
+                        ),
                     )[1];
                 };
             }