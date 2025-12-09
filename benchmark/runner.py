scylladb-zpp-2024-javascript-driver/scylladb-javascript-driver

Add new benchmark - deserialization types
uuid, int, timeuuid, inet, date, time

benchmark/runner.py
@@ -59,6 +59,7 @@ n_min["concurrent_insert.js"] = 4_000_000 / 64
 n_min["concurrent_select.js"] = 400_000 / 64
 n_min["insert.js"] = 400_000 / 64
 n_min["select.js"] = 100_000 / 64
+n_min["deser.js"] = 2_000 / 64
 
 steps = {}
 
@@ -67,14 +68,14 @@ step = 4
 # --------- libs and rust benchmark names ----------
 libs = ["scylladb-javascript-driver", "cassandra-driver"]
 benchmarks = ["concurrent_insert.js", "insert.js", "select.js",
-              "concurrent_select.js"]
+              "concurrent_select.js", "deser.js"]
 
 name_rust = {}
 name_rust["concurrent_insert.js"] = "concurrent_insert_benchmark"
 name_rust["insert.js"] = "insert_benchmark"
 name_rust["select.js"] = "select_benchmark"
 name_rust["concurrent_select.js"] = "concurrent_select_benchmark"
-
+name_rust["deser.js"] = "deser_benchmark"
 
 df = {}
 df_mem = {}

scylladb/nodejs-rs-driver

benchmark/runner.py
@@ -59,6 +59,7 @@ n_min["concurrent_insert.js"] = 4_000_000 / 64
 n_min["concurrent_select.js"] = 400_000 / 64
 n_min["insert.js"] = 400_000 / 64
 n_min["select.js"] = 100_000 / 64
+n_min["deser.js"] = 2_000 / 64
 n_min["batch.js"] = 3_000_000 / 64
 n_min["paging.js"] = 10_000 / 64
 n_min["large_select.js"] = 4_000 / 64
@@ -71,13 +72,14 @@ step = 4
 libs = ["scylladb-nodejs-rs-driver", "cassandra-driver"]
 benchmarks = ["concurrent_insert.js", "insert.js", "select.js",
              "concurrent_select.js", "batch.js", "paging.js",
-             "large_select.js"]
+             "large_select.js", "deser.js"]
 
 name_rust = {}
 name_rust["concurrent_insert.js"] = "concurrent_insert_benchmark"
 name_rust["insert.js"] = "insert_benchmark"
 name_rust["select.js"] = "select_benchmark"
 name_rust["concurrent_select.js"] = "concurrent_select_benchmark"
+name_rust["deser.js"] = "deser_benchmark"
 name_rust["batch.js"] = "batch_benchmark"
 name_rust["paging.js"] = "paging_benchmark"
 name_rust["large_select.js"] = "large_select_benchmark"