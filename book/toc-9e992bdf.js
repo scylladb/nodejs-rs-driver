004926e5d63994fc6044cdaf84c4713d4fce7e5a
adespawn <adespawn@users.noreply.github.com>
2026-01-21 08:54:07 +0000

    deploy: 34b62ba9711d0441a6e9c02f4786799caeed4e58

--- book/toc-9e992bdf.js
+++ book/toc-9e992bdf.js
@@ -8,7 +8,7 @@ class MDBookSidebarScrollbox extends HTMLElement {
         super();
     }
     connectedCallback() {
-        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><span class="chapter-link-wrapper"><a href="shutdown.html"><strong aria-hidden="true">1.</strong> Shutdown</a></span></li><li class="chapter-item expanded "><span class="chapter-link-wrapper"><a href="migration_guide.html"><strong aria-hidden="true">2.</strong> Migration guide</a></span></li><li class="chapter-item expanded "><span class="chapter-link-wrapper"><a href="internal/SUMMARY.html">Documentation</a></span></li></ol>';
+        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><span class="chapter-link-wrapper"><a href="shutdown.html"><strong aria-hidden="true">1.</strong> Shutdown</a></span></li><li class="chapter-item expanded "><span class="chapter-link-wrapper"><a href="migration_guide.html"><strong aria-hidden="true">2.</strong> Migration guide</a></span></li><li class="chapter-item expanded "><span class="chapter-link-wrapper"><a href="load_balancing.html"><strong aria-hidden="true">3.</strong> Load balancing</a></span></li><li class="chapter-item expanded "><span class="chapter-link-wrapper"><a href="internal/SUMMARY.html">Documentation</a></span></li></ol>';
         // Set the current, active page, and reveal it if it's hidden
         let current_page = document.location.href.toString().split('#')[0].split('?')[0];
         if (current_page.endsWith('/')) {
