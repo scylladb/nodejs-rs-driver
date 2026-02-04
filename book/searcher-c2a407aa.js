a8c810cfb31e6ec0424949e622c4d5295cac885c
adespawn <adespawn@users.noreply.github.com>
2026-02-04 08:49:43 +0000

    deploy: d6c8210310ad908b3e80e5f678019cad4f014720

--- book/searcher-c2a407aa.js
+++ book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-54525e77.js',
+                path_to_root + 'searchindex-d0d1e991.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
