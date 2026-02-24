8ca5ae22bc013c41b18cacf2fefa3c1f94e54f5b
David Garcia <david@techdocs.studio>
2026-02-24 10:41:46 +0000

    fix: multiversion

--- docs/_utils/multiversion.sh
+++ docs/_utils/multiversion.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd .. && sphinx-multiversion docs/source docs/_build/dirhtml \
-    --pre-build 'cd .. && npm install && npm run js-doc'
+    --pre-build 'cd .. && npm install && npm run js-doc && python docs/_utils/generate_api_pages.py'
