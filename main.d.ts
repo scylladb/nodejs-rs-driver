70b4c3e52e98d2d25949d94f1e79dbe6f44c7679
Stanisław Czech <stasio.czech@gmail.com>
2026-03-09 13:48:46 +0100

    Update client options Typescript definitions
    Before id, applicationName and applicationVersion were in the DseClientOptions
    TS class, despite being defined in the ClientOptions in the JS code.
    This commit moves those properties to the ClientOptions class in the TS code.

--- main.d.ts
+++ main.d.ts
@@ -29,7 +29,7 @@ export class Client extends events.EventEmitter {
   metadata: metadata.Metadata;
   metrics: metrics.ClientMetrics;
 
-  constructor(options: DseClientOptions);
+  constructor(options: ClientOptions);
 
   connect(): Promise<void>;
 
@@ -240,6 +240,9 @@ export interface ClientOptions {
     tcpNoDelay?: boolean;
   };
   sslOptions?: SslOptions;
+  id?: Uuid;
+  applicationName?: string;
+  applicationVersion?: string;
 }
 
 export interface SslOptions {
@@ -279,12 +282,6 @@ export interface QueryOptions {
   serialConsistency?: number;
   timestamp?: number | Long;
   traceQuery?: boolean;
-}
-
-export interface DseClientOptions extends ClientOptions {
-  id?: Uuid;
-  applicationName?: string;
-  applicationVersion?: string;
 }
 
 export class ExecutionProfile {
