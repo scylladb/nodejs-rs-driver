0140e47c26f6323442390466a60d256350f17305
Stanisław Czech <stanislaw.czech@scylladb.com>
2026-04-13 16:06:49 +0200

    Implement converter from HashMaps into JS objects
    This is done as a new class that wrap over the existing maps.

--- src/utils/to_napi_obj.rs
+++ src/utils/to_napi_obj.rs
@@ -1,3 +1,10 @@
+use std::{collections::HashMap, marker::PhantomData};
+
+use napi::{
+    Env, JsValue,
+    bindgen_prelude::{JsObjectValue, Object, ToNapiValue},
+};
+
 /// This macro creates a struct with a defined list of field,
 /// that can be used as an return value to JS exposed function.
 ///
@@ -57,6 +64,65 @@ macro_rules! define_rust_to_js_convertible_object {
         }
 
     };
+}
+
+/// Structure that converts a Rust Map into a JS object
+/// You can provide it with values that already implement ToNapiValue,
+/// or values that can be converted to wrappers using Into trait.
+///
+/// - K is the key type, must be convertible to string
+/// - V is the type of value you provided at the construction
+/// - W is the wrapper type, that has to implement From<V> trait (and ToNapiValue)
+///
+/// By default W = V
+pub struct NamedMap<K, V, W = V>
+where
+    // Note, that JS allows objects keyed by something other than strings
+    // For now we do not need such functionality
+    K: AsRef<str>,
+    W: ToNapiValue,
+    V: Into<W>,
+{
+    pub(crate) map: HashMap<K, V>,
+    _p: PhantomData<W>,
+}
+
+impl<K, V, W> NamedMap<K, V, W>
+where
+    K: AsRef<str>,
+    W: ToNapiValue,
+    V: Into<W>,
+{
+    pub(crate) fn new(v: HashMap<K, V>) -> Self {
+        NamedMap {
+            map: v,
+            _p: PhantomData,
+        }
+    }
+}
+
+impl<K, V, W> ToNapiValue for NamedMap<K, V, W>
+where
+    K: AsRef<str>,
+    W: ToNapiValue,
+    V: Into<W>,
+{
+    /// # Safety
+    ///
+    /// Valid pointer to napi env must be provided
+    unsafe fn to_napi_value(
+        env: napi::sys::napi_env,
+        val: Self,
+    ) -> napi::Result<napi::sys::napi_value> {
+        let env = Env::from_raw(env);
+        let mut obj = Object::new(&env)?;
+
+        for (key, val) in val.map.into_iter() {
+            obj.set_named_property(key.as_ref(), val.into())?;
+        }
+
+        Ok(obj.raw())
+    }
 }
 
 pub(crate) use define_rust_to_js_convertible_object;
