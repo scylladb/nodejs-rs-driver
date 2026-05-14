edff7b2682778f6965fafd9f32e8e44a5dec33fa
Stanisław Czech <stanislaw.czech@scylladb.com>
2026-05-14 14:18:33 +0200

    Convert execution profiles to TS
    This commit is LLM generated change from JS to TS.
    No logic changes should be introduced in this commit.

--- lib/execution-profile.ts
+++ lib/execution-profile.ts
@@ -1,10 +1,11 @@
-// @ts-nocheck
-"use strict";
+import types = require("./types");
+import promiseUtils = require("./promise-utils");
+import newUtils = require("./new-utils");
+import type { loadBalancing, retry } from "./policies";
+import type { Client, ClientOptions, EmptyCallback, Host, HostMap } from "../";
 
-const utils = require("./utils");
-const types = require("./types");
-const promiseUtils = require("./promise-utils");
-const { throwNotSupported } = require("./new-utils");
+type LoadBalancingPolicy = loadBalancing.LoadBalancingPolicy;
+type RetryPolicy = retry.RetryPolicy;
 
 /**
  * Represents a set configurations to be used in a statement execution to be used for a single {@link Client} instance.
@@ -25,60 +26,51 @@ const { throwNotSupported } = require("./new-utils");
  * client.execute(query, params, { executionProfile: 'metrics-oltp' }, callback);
  */
 class ExecutionProfile {
+    /** Name of the execution profile. */
+    name: string;
+    /** Consistency level. */
+    consistency?: types.consistencies;
+    /** Load-balancing policy. */
+    loadBalancing?: LoadBalancingPolicy;
+    /** Client read timeout. */
+    readTimeout?: number;
+    /** Retry policy. */
+    retry?: RetryPolicy;
+    /** Serial consistency level. */
+    serialConsistency?: types.consistencies;
+
     /**
-     * @param {String} name Name of the execution profile.
+     * @param name Name of the execution profile.
      * Use `'default'` to specify that the new instance should be the default {@link ExecutionProfile} if no
      * profile is specified in the execution.
-     * @param {Object} [options] Profile options, when any of the options is not specified the {@link Client} will the use
+     * @param options Profile options, when any of the options is not specified the {@link Client} will the use
      * the ones defined in the default profile. See {@link ClientOptions} for more details.
-     * @param {Number} [options.consistency] The consistency level to use for this profile.
-     * @param {LoadBalancingPolicy} [options.loadBalancing] The load-balancing policy to use for this profile.
-     * @param {Number} [options.readTimeout] The client per-host request timeout to use for this profile.
-     * @param {RetryPolicy} [options.retry] The retry policy to use for this profile.
-     * @param {Number} [options.serialConsistency] The serial consistency level to use for this profile.
      */
-    constructor(name, options) {
+    constructor(name: string, options?: {
+        consistency?: types.consistencies;
+        loadBalancing?: LoadBalancingPolicy;
+        readTimeout?: number;
+        retry?: RetryPolicy;
+        serialConsistency?: types.consistencies;
+    }) {
+        // Legacy check. Old code had this check, and using TS does not prevent JS user from providing wrong type
         if (typeof name !== "string") {
             throw new TypeError("Execution profile name must be a string");
         }
-        options = options || utils.emptyObject;
-        /**
-         * Name of the execution profile.
-         * @type {String}
-         */
+        const opts = options || {} as NonNullable<typeof options>;
         this.name = name;
-        /**
-         * Consistency level.
-         * @type {Number | undefined}
-         */
-        this.consistency = options.consistency;
-        /**
-         * Load-balancing policy
-         * @type {LoadBalancingPolicy}
-         */
-        this.loadBalancing = options.loadBalancing;
-        /**
-         * Client read timeout.
-         * @type {Number| undefined}
-         */
-        this.readTimeout = options.readTimeout;
-        /**
-         * Retry policy.
-         * @type {RetryPolicy}
-         */
-        this.retry = options.retry;
-        /**
-         * Serial consistency level.
-         * @type {Number | undefined}
-         */
-        this.serialConsistency = options.serialConsistency;
+        this.consistency = opts.consistency;
+        this.loadBalancing = opts.loadBalancing;
+        this.readTimeout = opts.readTimeout;
+        this.retry = opts.retry;
+        this.serialConsistency = opts.serialConsistency;
 
         // @ts-ignore - This is an explicit check for the use of deprecated option
-        if (options.graphOptions !== undefined) {
+        if (opts.graphOptions !== undefined) {
             // This option was present in the DSx driver, but is no longer relevant.
             // We explicitly check for it to inform users using this options,
             // to avoid any confusion if the user code depends on this configuration.
-            throwNotSupported("Graph options");
+            newUtils.throwNotSupported("Graph options");
         }
     }
 }
@@ -88,45 +80,39 @@ class ExecutionProfile {
  * @ignore
  */
 class ProfileManager {
-    /**
-     * @param {ClientOptions} options
-     */
-    constructor(options) {
+    _profiles: ExecutionProfile[];
+    _defaultConfiguredRetryPolicy: RetryPolicy | undefined;
+    _defaultProfile!: ExecutionProfile;
+    /** A array of unique load balancing policies */
+    _loadBalancingPolicies: LoadBalancingPolicy[];
+    /** A dictionary of name keys and profile values */
+    _profilesMap: Record<string, ExecutionProfile>;
+    /** A dictionary of name keys and custom payload dictionaries as values */
+    _customPayloadCache: Record<string, never>;
+
+    constructor(options: ClientOptions) {
         this._profiles = options.profiles || [];
         this._defaultConfiguredRetryPolicy = undefined;
         this._setDefault(options);
-        // A array of unique load balancing policies
-        /**
-         * @type {any[]}
-         */
         this._loadBalancingPolicies = [];
-        // A dictionary of name keys and profile values
-        /**
-         * @type {Record<string | number, any>}
-         */
         this._profilesMap = {};
-        // A dictionary of name keys and custom payload dictionaries as values
         this._customPayloadCache = {};
-        this._profiles.forEach( (/** @type {{ name: string | number; loadBalancing: any; }} */ p) => {
+        this._profiles.forEach((p) => {
             this._profilesMap[p.name] = p;
             // Set required properties
             p.loadBalancing =
                 p.loadBalancing || this._defaultProfile.loadBalancing;
             // Using array indexOf is not very efficient (O(n)) but the amount of profiles should be limited
             // and a handful of load-balancing policies (no hashcode for load-Balancing policies)
-            if (this._loadBalancingPolicies.indexOf(p.loadBalancing) === -1) {
-                this._loadBalancingPolicies.push(p.loadBalancing);
+            if (this._loadBalancingPolicies.indexOf(p.loadBalancing!) === -1) {
+                this._loadBalancingPolicies.push(p.loadBalancing!);
             }
         });
     }
 
-    /**
-     * @param {Client} client
-     * @param {HostMap} hosts
-     */
-    async init(client, hosts) {
+    async init(client: Client, hosts: HostMap): Promise<void> {
         for (const lbp of this._loadBalancingPolicies) {
-            await promiseUtils.fromCallback((/** @type {any} */ callback) =>
+            await promiseUtils.fromCallback((callback: EmptyCallback) =>
                 lbp.init(client, hosts, callback),
             );
         }
@@ -134,10 +120,9 @@ class ProfileManager {
 
     /**
      * Uses the load-balancing policies to get the relative distance to the host and return the closest one.
-     * @param {Host} host
      */
-    getDistance(host) {
-        let distance = types.distance.ignored;
+    getDistance(host: Host & { setDistance(d: types.distance): void }): types.distance {
+        let distance: types.distance = types.distance.ignored;
         // this is performance critical: we can't use any other language features than for-loop :(
         for (let i = 0; i < this._loadBalancingPolicies.length; i++) {
             const d = this._loadBalancingPolicies[i].getDistance(host);
@@ -154,32 +139,25 @@ class ProfileManager {
     }
 
     /**
-     * @param {String|ExecutionProfile} [name]
-     * @returns {ExecutionProfile|undefined} It returns the execution profile by name or the default profile when name is
-     * undefined. It returns undefined when the profile does not exist.
+     * Returns the execution profile by name or the default profile when name is
+     * undefined. Returns undefined when the profile does not exist.
      */
-    getProfile(name) {
+    getProfile(name?: string | ExecutionProfile): ExecutionProfile | undefined {
         if (name instanceof ExecutionProfile) {
             return name;
         }
         return this._profilesMap[name || "default"];
     }
 
-    /** @returns {ExecutionProfile} */
-    getDefault() {
+    getDefault(): ExecutionProfile {
         return this._defaultProfile;
     }
 
-    /** @returns {LoadBalancingPolicy} */
-    getDefaultLoadBalancing() {
+    getDefaultLoadBalancing(): LoadBalancingPolicy | undefined {
         return this._defaultProfile.loadBalancing;
     }
 
-    /**
-     * @private
-     * @param {ClientOptions} options
-     */
-    _setDefault(options) {
+    private _setDefault(options: ClientOptions): void {
         this._defaultProfile = this._profiles.filter(function (p) {
             return p.name === "default";
         })[0];
@@ -195,25 +173,24 @@ class ProfileManager {
         // Set the required properties
         this._defaultProfile.loadBalancing =
             this._defaultProfile.loadBalancing ||
-            options.policies.loadBalancing;
+            options.policies?.loadBalancing;
         this._defaultProfile.retry =
-            this._defaultProfile.retry || options.policies.retry;
+            this._defaultProfile.retry || options.policies?.retry;
     }
 
     /**
      * Gets all the execution profiles currently defined.
-     * @returns {Array.<ExecutionProfile>}
      */
-    getAll() {
+    getAll(): ExecutionProfile[] {
         return this._profiles;
     }
 
-    getDefaultConfiguredRetryPolicy() {
+    getDefaultConfiguredRetryPolicy(): RetryPolicy | undefined {
         return this._defaultConfiguredRetryPolicy;
     }
 }
 
-module.exports = {
+export = {
     ProfileManager,
     ExecutionProfile,
 };
