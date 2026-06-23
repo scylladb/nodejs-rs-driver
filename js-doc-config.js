d951d0dfc472624c3c988c126e4a262aa5c13106
David Garcia <david@techdocs.studio>
2026-06-23 08:45:02 +0100

    docs: exclude package-private members from API reference

--- js-doc-config.js
+++ js-doc-config.js
@@ -4,5 +4,9 @@ module.exports = {
     plugins: ['plugins/markdown'],
     opts: {
         destination: "./public/docs",
+        // Exclude @package members from the public API documentation.
+        // JSDoc renders all access levels except @private by default, so we
+        // explicitly list the levels to render and leave "package" out.
+        access: ["public", "protected", "undefined"],
     }
 }
