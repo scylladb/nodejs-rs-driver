9bc543a2d8b61bf44cb5cc4203cfb888b537b5f3
hmarszalek <hanna.marszalek@scylladb.com>
2026-08-10 22:54:41 +0200

    Convert table-metadata to typescript

--- lib/metadata/table-metadata.ts
+++ lib/metadata/table-metadata.ts
@@ -1,27 +1,22 @@
-// @ts-nocheck
 "use strict";
 
-// Used for JS doc
-// eslint-disable-next-line no-unused-vars
-const { ColumnInfo } = require("../types/cql-utils");
+import { ColumnInfo } from "../types/cql-utils";
 
 /**
  * Some columns have a specific meaning in the context of a table,
  * and this meaning is represented by the {@link ColumnKind} enum.
- * @readonly
- * @enum {number}
  * @alias module:metadata~ColumnKind
  */
-const ColumnKind = {
+enum ColumnKind {
     /** Just a regular column. */
-    Regular: 0,
+    Regular = 0,
     /** Column that has the same value for all rows in a partition. */
-    Static: 1,
+    Static = 1,
     /** Column that is part of the clustering key. */
-    ClusteringKey: 2,
+    ClusteringKey = 2,
     /** Column that is part of the partition key. */
-    PartitionKey: 3,
-};
+    PartitionKey = 3,
+}
 
 /**
  * Describes a column of the table.
@@ -30,15 +25,22 @@ const ColumnKind = {
 class ColumnMetadata {
     /**
      * CQL type that the value stored in this column has.
-     * @type {ColumnInfo}
      */
-    type;
+    type: ColumnInfo;
 
     /**
      * Describes role of the column in the table.
-     * @type {ColumnKind}
      */
-    kind;
+    kind: ColumnKind;
+
+    /**
+     * @internal
+     * @ignore
+     */
+    constructor(typ: ColumnInfo, kind: ColumnKind) {
+        this.type = typ;
+        this.kind = kind;
+    }
 }
 
 /**
@@ -50,31 +52,41 @@ class TableMetadata {
      * Columns that constitute the table, keyed by column name.
      *
      * This type does not contain information about the order of the columns in the table.
-     * @type {Object.<string, ColumnMetadata>}
      */
-    columns;
+    columns: Record<string, ColumnMetadata>;
 
     /**
      * Names of the columns that constitute the partition key.
      * All names are guaranteed to be present in {@link columns}.
-     * @type {Array.<string>}
      */
-    partitionKey;
+    partitionKey: string[];
 
     /**
      * Names of the columns that constitute the clustering key.
      * All names are guaranteed to be present in {@link columns}.
-     * @type {Array.<string>}
      */
-    clusteringKey;
+    clusteringKey: string[];
 
     /**
      * Name of the partitioner used by the table, or null if not set.
-     * @type {string|null}
      */
-    partitioner;
+    partitioner: string | null;
+
+    /**
+     * @internal
+     * @ignore
+     */
+    constructor(
+        columns: Record<string, ColumnMetadata>,
+        partitionKey: string[],
+        clusteringKey: string[],
+        partitioner: string | null,
+    ) {
+        this.columns = columns;
+        this.partitionKey = partitionKey;
+        this.clusteringKey = clusteringKey;
+        this.partitioner = partitioner;
+    }
 }
 
-module.exports.TableMetadata = TableMetadata;
-module.exports.ColumnMetadata = ColumnMetadata;
-module.exports.ColumnKind = ColumnKind;
+export { TableMetadata, ColumnMetadata, ColumnKind };
